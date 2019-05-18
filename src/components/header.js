be5b5f07cc52e9c2b46a64873c4f633b57e7a17d
unknown <you@example.comsudiptokush@gmail.com>
2019-05-19 02:58:12 +0530

    changes

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,28 @@ import React from "react";
 import { Link } from "gatsby"
 import {imageProcessor} from "../hooks/image-processor-hook";
 import headerStyles from './header.module.scss';
+import {Navbar, Nav, NavDropdown} from "react-bootstrap";
 
 const Header = () => {
     const temp = imageProcessor("logo","fixed");
-    return <div className= {headerStyles.headerContainer}>
+    return <Navbar expand="lg" className="navi">
+            <Navbar.Brand href="#home">{temp}</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                <Nav.Link><Link to="/about">About</Link></Nav.Link>
+                <Nav.Link><Link to="/projects/">Projects</Link></Nav.Link>
+                <Nav.Link><Link to="/contactUs/">Contacts</Link></Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+    
+}
+export default Header;
+
+
+{/* <div className= {headerStyles.headerContainer}>
         <div className={headerStyles.brandLogo}>
           <Link to="/">
             {temp}
@@ -18,9 +36,4 @@ const Header = () => {
             <Link to="/projects/">Projects</Link>
             <Link to="/contactUs/">Contacts</Link>
         </div>
-        {/* <div className={headerStyles.brandName}>
-          Kush Infotech
-        </div> */}
-    </div>
-}
-export default Header;
+    </div> */}
