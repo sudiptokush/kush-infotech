c95ec9e35e7f1a40392a95651fdabd7055eb4843
Sudipto Mukherjee <sudiptokush@gmail.com>
2019-05-20 13:39:26 +0530

    changes

--- src/components/header.js
+++ src/components/header.js
@@ -2,38 +2,24 @@ import React from "react";
 import { Link } from "gatsby"
 import {imageProcessor} from "../hooks/image-processor-hook";
 import headerStyles from './header.module.scss';
-import {Navbar, Nav, NavDropdown} from "react-bootstrap";
+import {Navbar,Nav} from "react-bootstrap";
+
 
 const Header = () => {
     const temp = imageProcessor("logo","fixed");
-    return <Navbar expand="lg" className="navi">
+    return <Navbar expand="lg">
             <Navbar.Brand href="#home">{temp}</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link><Link to="/">Home</Link></Nav.Link>
-                <Nav.Link><Link to="/about">About</Link></Nav.Link>
-                <Nav.Link><Link to="/projects/">Projects</Link></Nav.Link>
-                <Nav.Link><Link to="/contactUs/">Contacts</Link></Nav.Link>
+              <Nav className="ml-auto">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/projects/">Projects</Link>
+                <Link to="/contactUs/">Contacts</Link>
               </Nav>
             </Navbar.Collapse>
+            
           </Navbar>
     
 }
 export default Header;
-
-
-{/* <div className= {headerStyles.headerContainer}>
-        <div className={headerStyles.brandLogo}>
-          <Link to="/">
-            {temp}
-          </Link> 
-          
-        </div>
-        <div className={headerStyles.menuBar}>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/projects/">Projects</Link>
-            <Link to="/contactUs/">Contacts</Link>
-        </div>
-    </div> */}
