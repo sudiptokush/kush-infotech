7522e0defa314be951bfc9d4169802d2dfc8c55d
unknown <you@example.comsudiptokush@gmail.com>
2019-06-20 16:47:18 +0530

    sudipto

---
+++ src/assets/project-details.config.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+export const projectDetailsConfig = {
+    kyokushinDetails: () => kyokushinDetails,
+    capitalDetails: () => capitalDetails,
+    selfandbeyondDetails: () => selfandbeyondDetails,
+    assetDetails: () => assetDetails,
+    payrollDetails: () => payrollDetails,
+    accountsDetails: () => accountsDetails,
+    eshopDetails: () => eshopDetails,
+    medicalDetails: () => medicalDetails,
+    ornetDetails: () => ornetDetails
+};
+
+const kyokushinDetails = <p> This site is an introductory static site for a Karate club imparting training in Kyokushin Karate.
+                             The site shows how Kyokushin Karate came into existence, its motive, rules and regulations,
+                             policies and its history. The site is full of image galleries and is technically very fast in loading
+                             the images. The site is made using React.js technology with Gatsby framework.
+                         </p>
+
+const capitalDetails = <p> The site is the official informative static web site of a retail company. It contains a fixed product
+                           catalogue along with discount offers in the home page. Site is full of several image galleries, which load
+                           at lightning speed, thanks to React Gatsby framework along with GraphQL technology.
+                       </p>
+
+const selfandbeyondDetails = <p> This static site is about a psychotherapy Well Being Center. They conduct several classes on well-
+                                 being and details of all such details appear on site. Several day-today events are also listed on
+                                 the site. Multiple photo galleries are used through carousals. The site is created using Angular-4.
+                             </p>
+
+const assetDetails = <p> This is a web application created using variety of Microsoft Technologies. A complete business of rentals
+                        of computers, printers and laptops is run from this web application. This is a complete application for managing 
+                        assets along with day-to-day transaction entries and business reports. Data is stored in Sql Server database.
+                     </p>
+
+const payrollDetails = <p> This is a complete multi company payroll web application written in Microsoft ASP.net and Silverlight.</p>
+
+const accountsDetails = <p> This is a complete financial accounting package consisting of accounts payable, accounts receivable, final
+                            accounts, Expenses, Journals and contra entries. Software was developed using PowerBuilder and Sql
+                            Anywhere.
+                        </p>
+
+const eshopDetails = <p> This is a demo application in Angular 6 and PostgreSql. This mimic an e-commerce application consisting
+                         of display of products along with categories in a tree. You can drill down the product details, place the
+                         orders through a shopping cart implementation along with many more features. Main attraction is
+                         unlimited product displays in a web page along with their respective images and details.
+                    </p>
+
+const medicalDetails = <p> This software was developed for a hospital in US to track the medicines administered on patients. The
+                           solution is highly refractured due to confidentiality agreement. The software was made using Angular 4
+                           and D3.js.
+                        </p>
+
+const ornetDetails = <p> This is a productivity tool created in React.js. Ornate can generate on the fly dynamic data entry forms
+                         from JSON objects. You can provide JSON objects to Ornate at run time and it will generate
+                         corresponding data entry form along with all sort of validations. Main attraction is it can be used with
+                         any library such as Material, Bootstrap and many more. Ornate allows unlimited level of nesting of
+                         controls. Repeater controls able to create and remove form controls at run time using ‘+’ or ‘-‘symbols
+                         are also available. You can also have unlimited level of nesting of repeater controls with Ornate. Ornate
+                         is very useful when it is required to generate thousands of data entry forms at run time. On click of
+                         Submit button, Ornate captures all data as JSON and submits it to server.
+                     </p>
--- src/pages/project-details.js
+++ src/pages/project-details.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../components/layout";
 import {navigate} from "gatsby";
 import { useGallery } from '../hooks/gallery-hook';
+import {projectDetailsConfig} from "../assets/project-details.config";
 
 const ProjectDetails = ({location}) => {
     
@@ -15,65 +16,7 @@ const ProjectDetails = ({location}) => {
                 {projectImages}
             </div>
             <div className="content">
-                <h4>Contents under construction</h4>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tristique mollis magna ut 
-                    rutrum. Nullam blandit aliquam nulla, nec consectetur nibh finibus id. Proin ultricies, nulla quis 
-                    viverra iaculis, ipsum magna luctus velit, non accumsan nisl nibh eu ante. Vivamus dictum rutrum 
-                    arcu, vel mollis nisl dignissim id. Ut at odio ac quam tincidunt elementum non ut tortor. In hac 
-                    habitasse platea dictumst. Pellentesque vel ornare erat. Mauris eu lectus faucibus neque malesuada 
-                    maximus ut vitae augue. Proin aliquet feugiat massa, sed pulvinar nisl pellentesque suscipit. Etiam 
-                    pharetra mauris neque, eu aliquet nulla mollis in. Nulla ornare eget velit at fermentum. Morbi 
-                    iaculis sem vel velit feugiat, id eleifend urna bibendum. Praesent rhoncus, eros nec cursus ornare, 
-                    dui neque feugiat felis, vel auctor risus nibh a turpis. Aliquam quis magna sem.
-                </p>
-
-                <p>
-                    Curabitur vitae quam sit amet ex ornare ornare ut eu odio. Maecenas hendrerit luctus orci, sit amet 
-                    faucibus lacus semper sed. Quisque rutrum sapien est, hendrerit interdum metus ultrices ut. Proin 
-                    efficitur vitae tortor vel congue. Integer nec interdum risus. Phasellus scelerisque quam placerat, 
-                    scelerisque massa sit amet, mollis libero. Donec in tellus ac elit aliquet vestibulum a vel ante. 
-                    Aenean efficitur lacus dapibus sem cursus, sit amet bibendum erat fermentum. In pharetra sem vel 
-                    orci scelerisque laoreet. Nulla vitae varius dui. Aliquam varius, lacus nec feugiat consequat, 
-                    est metus luctus velit, quis hendrerit urna nibh et augue. Cras tristique nibh sed lacus tristique, 
-                    vel dictum lorem dapibus.
-                </p>
-
-                <p>
-                    Vestibulum venenatis consectetur condimentum. Integer dolor enim, cursus vel fermentum quis, 
-                    viverra et ex. Aenean gravida sem non risus laoreet rhoncus sed eget velit. Quisque condimentum 
-                    ante quis libero mollis, at luctus augue lobortis. Nunc odio est, aliquet cursus felis sit amet, 
-                    vehicula suscipit neque. Phasellus ultrices erat eu ante lobortis, eu lobortis nisi tincidunt. 
-                    Aenean pulvinar augue id tortor viverra imperdiet. Nullam vitae volutpat metus. Donec tempus purus 
-                    et nisl posuere maximus. Aliquam pulvinar sapien in vulputate egestas. Nulla aliquam nisl lectus, 
-                    ac tincidunt ligula sagittis laoreet. Mauris sit amet quam non tortor bibendum posuere. Nullam 
-                    consequat fringilla sodales. Duis blandit odio elit. Nulla malesuada massa purus. Aenean sed dolor 
-                    et ipsum laoreet malesuada luctus nec sem.
-                </p>
-
-                <p>
-                    Aliquam posuere cursus feugiat. Cras justo massa, laoreet vel dictum at, pharetra at augue. Nam 
-                    felis velit, cursus vel mauris sed, pharetra convallis dolor. Aliquam massa elit, porttitor 
-                    tristique ultricies nec, porttitor eget justo. Quisque feugiat lobortis quam. Nullam eu rutrum 
-                    sem. Pellentesque quis sollicitudin felis, mattis ultrices erat. Suspendisse potenti. Phasellus a 
-                    dolor in purus eleifend congue. Curabitur elementum condimentum enim quis placerat. Etiam ut est 
-                    in arcu porttitor facilisis. Nullam sed nisl et urna tristique ullamcorper. Nulla eu neque euismod, 
-                    porttitor felis eget, vestibulum ex. Pellentesque placerat posuere massa, id fermentum sapien 
-                    interdum vel.
-                </p>
-
-                <p>
-                Nullam at purus ut diam finibus viverra at non purus. Praesent finibus iaculis dapibus. Donec laoreet, 
-                odio id sodales condimentum, mi velit maximus nibh, sit amet finibus elit mi et elit. Fusce cursus quam 
-                enim, ac aliquam nisl laoreet id. Donec non elit elementum, sollicitudin ex sed, egestas justo. Integer 
-                nec diam eu purus ornare vulputate. Etiam varius metus id justo faucibus fringilla nec vel felis. 
-                Vestibulum vehicula turpis sit amet turpis scelerisque ultricies. Cras tristique vel turpis quis 
-                ullamcorper. Nam vel justo a ligula consequat ornare. Duis placerat, ipsum et suscipit venenatis, 
-                magna leo fringilla felis, eu volutpat augue eros id lacus. Suspendisse vulputate ante velit, sit amet 
-                suscipit augue maximus ut. Aenean leo risus, rhoncus sit amet gravida et, dictum et metus. Cras eu sem 
-                turpis. Vivamus ac tincidunt purus. Phasellus posuere, urna quis sagittis vulputate, turpis augue congue 
-                metus, eget sollicitudin orci libero vitae ligula.
-                </p>
+                {projectDetailsConfig[details]()}
             </div>    
         </div>
     </Layout>
